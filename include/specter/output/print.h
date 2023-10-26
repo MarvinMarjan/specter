b8bfb6570a919d7ad79734a8592468557a81d108
Marvin Marjan <marvin.marjan@gmail.com>
2023-10-26 09:54:04 -0400

    add 'print.h'

---
+++ include/specter/output/print.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#include <specter/macro.h>
+
+
+
+SPECTER_NAMESPACE_BEGIN
+
+
+// print values to the terminal without newline at end
+template <typename... _ValueTy>
+inline void print(const _ValueTy&... values) noexcept {
+	(std::cout << ... << values);
+}
+
+
+// print values to the terminal with newline at end
+template <typename... _ValueTy>
+inline void println(const _ValueTy&... values) noexcept {
+	(std::cout << ... << values) << std::endl;
+}
+
+
+
+
+// print values to the terminal using wide-characters without newline at end
+template <typename... _ValueTy>
+inline void wprint(const _ValueTy&... values) noexcept {
+	(std::wcout << ... << values);
+}
+
+
+// print values to the terminal using wide-characters with newline at end
+template <typename... _ValueTy>
+inline void wprintln(const _ValueTy&... values) noexcept {
+	(std::wcout << ... << values) << std::endl;
+}
+
+
+SPECTER_NAMESPACE_END
